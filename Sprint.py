c2d18b5d57c54cae6047a33b58afe0a5a959dd0f
Jonah Greening <116300133+JonahJG@users.noreply.github.com>
2022-12-14 20:50:25 -0330

    Update Sprint.py
    Added option 8, fixed some bugs

--- Sprint.py
+++ Sprint.py
@@ -36,6 +36,313 @@ if Today.day == 1:
 
     f.close()
 
+  # Set up options as functions.
+
+def NewEmp():
+
+    pass
+
+
+def CompRev():
+
+    pass
+
+
+def CompExpenses():
+
+    pass
+
+
+def CarRentals():
+
+    pass
+
+
+def EmpPay():
+
+    pass
+
+
+def ProfList():
+
+    while True:
+
+        # Print out top of report
+
+        StartDate = date(2022, 12, 0o1)
+        Today = datetime.datetime.now()
+        print()
+        print("HAB TAXI SERVICES")
+        print(f"COMPANY PROFIT LISTING FROM {StartDate} TO {FV.FDateS(Today)} ")
+        print()
+        print("TRANSACTION  TRANSACTION   TRANSACTION      DRIVER     REVENUE      HST   TOTAL       TIPS ")
+        print("    ID          DATE       DESCRIPTION      NUMBER                        REVENUE")
+        print("===========================================================================================")
+
+        # Set counter to 0, set accumulator to 0
+        TransCtr = 0
+        TotRevAcc = 0
+
+        # Open Revenues.dat to read
+
+        f = open("Revenues.dat", "r")
+
+        for CustDataLine in f:
+            CustLine = CustDataLine.split(",")
+            TransDate = CustLine[1].strip()
+            TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+
+            if TransDateP.month == 12:
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransID = int(CustLine[0].strip())
+                TransDescrip = CustLine[2].strip()
+                DriverNo = int(CustLine[3].strip())
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotalRev = Total + Tips
+
+                print(f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotalRev
+
+        f.close()
+        print("===========================================================================================")
+        print(f"NO. TRANSACTIONS: {TransCtr:>3d}                                             TOTAL REVENUE: {FV.FDollar2(TotRevAcc):>9s}")
+        print()
+
+        Cont = input("ENTER 'END' TO QUIT: ").upper()
+        if Cont == "END":
+            break
+        else:
+            print("Please enter 'END' to quit.")
+
+
+def DriverFinList():
+
+    pass
+
+
+def EmpRevList():
+
+    while True:
+        # User input
+        print()
+        print("HAB TAXI SERVICES")
+        print("PRINT A DRIVER REVENUE REPORT")
+        print()
+        DriverNum = int(input("Enter the driver number: "))
+        # Print out top of report
+
+        StartDate = date(2022, 12, 0o1)
+        Today = datetime.datetime.now()
+        print()
+        print("HAB TAXI SERVICES")
+        print(f"EMPLOYEE REVENUE REPORT FROM {StartDate} TO {FV.FDateS(Today)} ")
+        print()
+        print("TRANSACTION  TRANSACTION   TRANSACTION      DRIVER     REVENUE      HST   TOTAL       TIPS ")
+        print("    ID          DATE       DESCRIPTION      NUMBER                        REVENUE")
+        print("===========================================================================================")
+
+        # Set counter to 0
+        TransCtr = 0
+        TotRevAcc = 0
+
+        # Open Revenues.dat to read
+
+        f = open("Revenues.dat", "r")
+
+        for CustDataLine in f:
+            CustLine = CustDataLine.split(",")
+            DriverNo = int(CustLine[3].strip())
+
+            if DriverNo == 1968 and DriverNum == 1968:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+            elif DriverNo == 1111 and DriverNum == 1111:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+            elif DriverNo == 2222 and DriverNum == 2222:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+
+            elif DriverNo == 3333 and DriverNum == 3333:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+            elif DriverNo == 2076 and DriverNum == 2076:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+            elif DriverNo == 1978 and DriverNum == 1978:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+            elif DriverNo == 2106 and DriverNum == 2106:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+            elif DriverNo == 2001 and DriverNum == 2001:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+            elif DriverNo == 2301 and DriverNum == 2301:
+                TransID = int(CustLine[0].strip())
+                TransDate = CustLine[1].strip()
+                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
+                TransDescrip = CustLine[2].strip()
+                TransAmt = float(CustLine[4].strip())
+                HST = float(CustLine[5].strip())
+                Total = float(CustLine[6].strip())
+                Tips = float(CustLine[-1].strip())
+
+                TransDescripDsp = "{:.15}".format(TransDescrip)
+                TotRev = Total + Tips
+
+                print(
+                    f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
+                TransCtr += 1
+                TotRevAcc += TotRev
+
+        f.close()
+        print("===========================================================================================")
+        print(f"NO. TRANSACTIONS: {TransCtr:>3d}                                             TOTAL REVENUE: {FV.FDollar2(TotRevAcc):>9s}")
+
+        print()
+        Cont = input("ENTER 'END' TO QUIT: ").upper()
+        if Cont == "END":
+            break
+        else:
+            print("Please enter 'END' to quit.")
+
+
 # Menu set-up
 
 while True:
@@ -49,98 +356,9 @@ while True:
     print("5. Record Employee Payment.")
     print("6. Print Company Profit Listing")
     print("7. Print Driver Financial Listing.")
-    print("8. (TBD)")
+    print("8. Print Driver Revenue Report.")
     print("9. Quit Program.")
     print()
-
-    # Set up options as functions.
-
-    def NewEmp():
-
-        pass
-
-    def CompRev():
-
-        pass
-
-    def CompExpenses():
-
-        pass
-
-    def CarRentals():
-
-        pass
-
-    def EmpPay():
-
-        pass
-
-    def ProfList():
-
-        while True:
-
-            # User input
-
-            Driver = input("Enter the driver number (####): ")
-
-            # Print out top of report
-
-            StartDate = date(2022, 12, 0o1)
-            Today = datetime.datetime.now()
-            print()
-            print("HAB TAXI SERVICES")
-            print(f"COMPANY PROFIT LISTING FROM {StartDate} to {FV.FDateS(Today)} ")
-            print()
-            print("TRANSACTION  TRANSACTION   TRANSACTION      DRIVER     REVENUE      HST   TOTAL       TIPS ")
-            print("    ID          DATE       DESCRIPTION      NUMBER                        REVENUE")
-            print("===========================================================================================")
-
-            # Set counter to 0, set accumulator to 0
-            TransCtr = 0
-
-            # Open Revenues.dat to read
-
-            f = open("Revenues.dat", "r")
-
-            for CustDataLine in f:
-                CustLine = CustDataLine.split(",")
-                TransDate = CustLine[1].strip()
-                TransDateP = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
-
-                if TransDateP.month == 12:
-                    TransDate = datetime.datetime.strptime(TransDate, "%Y-%m-%d")
-                    TransID = int(CustLine[0].strip())
-                    TransDescrip = CustLine[2].strip()
-                    DriverNo = int(CustLine[3].strip())
-                    TransAmt = float(CustLine[4].strip())
-                    HST = float(CustLine[5].strip())
-                    Total = float(CustLine[6].strip())
-                    Tips = float(CustLine[-1].strip())
-
-                    TransDescripDsp = "{:.15}".format(TransDescrip)
-
-                    print(
-                        f"   {TransID:>4d}       {FV.FDateS(TransDate)}   {TransDescripDsp:<15s}  {DriverNo:<4d}    {FV.FDollar2(TransAmt):>9s}  {FV.FDollar2(HST):>9s} {FV.FDollar2(Total):>7s} {FV.FDollar2(Tips):>9s}")
-                    TransCtr += 1
-
-            f.close()
-            print("===========================================================================================")
-            print(f"NO. TRANSACTIONS: {TransCtr:>3d}")
-            print()
-
-            Cont = input("ENTER 'END' TO QUIT: ").upper()
-            if Cont == "END":
-                break
-            else:
-                print("Please enter 'END' to quit.")
-
-    def DriverFinList():
-
-        pass
-
-    def TBD():
-
-        pass
 
     Choice = int(input("Enter your choice (1-9): "))
 
@@ -174,10 +392,14 @@ while True:
 
     elif Choice == 8:
 
-        TBD()
+        EmpRevList()
 
     elif Choice == 9:
         break
+
+
+
+
 
 
 
